c344a61f649572090635107f341aa58cb178493c
HappyCodingMama <happycodingmama@gmail.com>
2021-03-05 21:42:04 +0900

    build the DataLayer

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,19 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Login from "./components/Login";
+import Player from "./components/Player";
 import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from "./components/DataLayer";
 import "./App.css";
 
+const spotify = new SpotifyWebApi();
+
 function App() {
+  const [{ user, token }, dispatch] = useDataLayerValue();
+
   useEffect(() => {
-    const token = getTokenFromUrl();
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+    }
+
     console.log("I have a Token : ", token);
   }, []);
 
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  console.log("a", user);
+  console.log("b", token);
+
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
